8aada82e5d95669964640b7d0d59c144b05246ea
laurabacon <laura_kessinger@yahoo.com>
2024-02-21 10:44:27 -0700

    updated deployed links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,13 +7,13 @@ import image4 from '../assets/StarRay.png';
 import image3 from '../assets/TechBlog.png';
 import image1 from '../assets/WeatherAPI.png';
 import image5 from '../assets/workDay.png';
-import image6 from '../assets/StarRay copy.png';
+import image6 from '../assets/RefactorStarRay.png';
 
 
 
 const Portfolio = () => {
   const titles = ['Weather Dahsboard', 'Date Night Planner', 'Tech Blog', 'Star Ray Design E-Commerce', 'Work Day Scheduler', 'Star Ray Design Refactored'];
-  const deployLinks = ['https://scook9.github.io/module-6-weather-dashboard/', 'https://scook9.github.io/menYou/' ,'https://tech-blog-8675309-7fdcc372d423.herokuapp.com/login', 'https://shielded-gorge-64229-3114d756159e.herokuapp.com/', 'https://laurabacon.github.io/Work-Day-Scheduler/', 'https://shielded-gorge-64229-3114d756159e.herokuapp.com/'];
+  const deployLinks = ['https://scook9.github.io/module-6-weather-dashboard/', 'https://scook9.github.io/menYou/' ,'https://tech-blog-8675309-7fdcc372d423.herokuapp.com/login', 'https://shielded-gorge-64229-3114d756159e.herokuapp.com/', 'https://laurabacon.github.io/Work-Day-Scheduler/', 'https://lit-eyrie-12834-5b6e0706190e.herokuapp.com/'];
   const githubLinks = ['https://github.com/scook9/module-6-weather-dashboard', 'https://github.com/scook9/menYou' ,'https://github.com/scook9/module-14-tech-blog', 'https://github.com/scook9/candles-n-things', 'https://github.com/laurabacon/Work-Day-Scheduler', 'https://github.com/laurabacon/Star-Ray'];
 
   const imageUrls = [
