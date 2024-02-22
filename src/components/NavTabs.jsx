4bcc4bd90b72622bed294bd91926adff51d34097
laurabacon <laura_kessinger@yahoo.com>
2024-02-21 20:59:13 -0700

    debugging navbar another test

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link, useLocation } from 'react-router-dom';
+
 
 function NavTabs({ setCurrentTab, currentTab }) {
   const handleTabChange = (tab) => {
@@ -7,51 +11,28 @@ function NavTabs({ setCurrentTab, currentTab }) {
   };
 
   const currentPage = useLocation().pathname;
-
+  
   return (
-    <ul className="nav nav-tabs">
-      {/* <li className="nav-item">
-        <Link
-          to="/"
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link' }
-          onClick={() => handleTabChange('Home')}
-        >
-          Home
-        </Link>
-      </li> */}
-      <li className="nav-item">
-        <Link
-          to="/About"
-          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Portfolio"
-          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Resume"
-          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Contact"
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <>
+      <Navbar expand={false} style={{ backgroundColor: '#f8f9fa' }}>
+        <Container fluid>
+          <Navbar.Brand href="#"></Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton></Offcanvas.Header>
+            <Offcanvas.Body className="justify-content-end flex-grow-1 pe-3">
+              <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
+              <Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
+              <Link to="/About" className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>About</Link>
+              <Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
