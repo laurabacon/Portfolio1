e3c7f974a5551000a7480b7d1c7f0cbb371e5929
laurabacon <laura_kessinger@yahoo.com>
2024-02-21 20:44:09 -0700

    working on issue

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Nav from 'react-bootstrap/Nav';
-import { useLocation } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom';
 
 function NavTabs({ setCurrentTab, currentTab }) {
   const handleTabChange = (tab) => {
@@ -12,50 +7,51 @@ function NavTabs({ setCurrentTab, currentTab }) {
   };
 
   const currentPage = useLocation().pathname;
-  
+
   return (
-    <>
-      <Navbar expand={false} style={{ backgroundColor: '#f8f9fa' }}>
-        <Container fluid>
-          <Navbar.Brand href="#"></Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton></Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link 
-                  href="/" 
-                  className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                >
-                  Home
-                </Nav.Link>
-                <Nav.Link 
-                  href="/Portfolio" 
-                  className={currentPage === "/Portfolio" ? "nav-link active" : "nav-link"}
-                >
-                  Portfolio
-                </Nav.Link>
-                <Nav.Link 
-                  href="/About" 
-                  className={currentPage === "/About" ? "nav-link active" : "nav-link"}
-                >
-                  About
-                </Nav.Link>
-                <Nav.Link 
-                  href="/Contact" 
-                  className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}
-                >
-                  Contact
-                </Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </>
+    <ul className="nav nav-tabs">
+      {/* <li className="nav-item">
+        <Link
+          to="/"
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link' }
+          onClick={() => handleTabChange('Home')}
+        >
+          Home
+        </Link>
+      </li> */}
+      <li className="nav-item">
+        <Link
+          to="/About"
+          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Portfolio"
+          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Resume"
+          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Contact"
+          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 }
 
